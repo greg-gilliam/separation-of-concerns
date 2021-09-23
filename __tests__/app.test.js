b25553ab7158e59a0bf2893f329508b28868fef4
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-09-22 17:57:22 -0700

    fixed twilio kinda

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,5 +1,5 @@
 const pool = require('../lib/utils/pool');
-const twilio = require('twilio');
+// const twilio = require('twilio');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
